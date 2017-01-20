3447c13e7b4d26b25a45ef8674cfcf43e7ad2483
Mikhail Savushkin <kulv3r@gmail.com>
2017-01-20 19:47:01 +0300

    first poor graph

--- pump_select/public/views.py
+++ pump_select/public/views.py
@@ -15,15 +15,33 @@ blueprint = Blueprint('public', __name__, static_folder='../static')
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def home():
-    Q = np.array(data.Q1)
     H = np.array(data.H1)
+    Q = np.array(data.Q1)
 
     points_series_data = [(q, data.H1[idx]) for idx, q in enumerate(data.Q1)]
 
     polynoms = {}
+    polynoms_vals = {}
     for n in (2, 4, 6):
-        polynom = np.polynomial.polynomial.polyfit(Q, H, n)
-        polynoms[n] = polynom.tolist()
+        polynom = np.polynomial.polynomial.polyfit(Q, H, n).tolist()
+        # It returns for an n=4
+        # [1.16035322e+02, -6.77159076e-04, 7.49052572e-06, -7.71847069e-08, 3.72781671e-11]
+        #       A                 B                 C              D               E
+        # so formula = A + Bx + Cx**2 + Dx**3 + Ex**4
+        x = data.Q1[0]
+        max_x = data.Q1[-1]
+        if max_x < x:
+            x, max_x = max_x, x
+        steps = len(data.Q1)
+        step = (max_x - x) / steps
+        series = []
+        while x < max_x:
+            # f = p[0] + p[1] * x**1 + p[2] * x**2 + ...
+            f = sum([(c * x**p) for p, c in enumerate(polynom)])
+            series.append((round(x, 1), round(f, 1)))
+            x += step
+
+        polynoms_vals[n] = series
 
     # form = CharacteristicValuesForm()
     # if form.validate_on_submit():
@@ -34,7 +52,7 @@ def home():
     return render_template(
         'public/home.html',
         # form=form,
-        polynoms=polynoms,
+        polynoms_vals=polynoms_vals,
         points_series_data=points_series_data,
     )
 
