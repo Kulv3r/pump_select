a260e2ce1a709319e3e6576bff1ae5cb70174157
Mikhail Savushkin <kulv3r@gmail.com>
2017-01-21 01:17:06 +0300

    ...

--- pump_select/assets.py
+++ pump_select/assets.py
@@ -3,17 +3,17 @@
 from flask_assets import Bundle, Environment
 
 css = Bundle(
-    'libs/font-awesome/css/font-awesome.min.css',
-    'libs/bootstrap/dist/css/bootstrap.min.css',
+    # 'libs/font-awesome/css/font-awesome.min.css',
+    # 'libs/bootstrap/dist/css/bootstrap.min.css',
     'css/style.css',
     filters='cssmin',
     output='public/css/common.css'
 )
 
 js = Bundle(
-    'libs/jQuery/dist/jquery.min.js',
-    'libs/bootstrap/dist/js/bootstrap.min.js',
-    'libs/highcharts/highcharts.js',
+    # 'libs/jQuery/dist/jquery.min.js',
+    # 'libs/bootstrap/dist/js/bootstrap.min.js',
+    # 'libs/highcharts/highcharts.js',
     'js/plugins.js',
     filters='jsmin',
     output='public/js/common.js'
